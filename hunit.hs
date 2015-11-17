98fc501f3631db96747c43156a0978130e4deff8
Bart Massey <bart@cs.pdx.edu>
2015-11-17 15:31:12 -0800

    extended hunit to test alternate implementations

--- hunit.hs
+++ hunit.hs
@@ -3,7 +3,7 @@
 -- Please see the file LICENSE in the source
 -- distribution of this software for license terms.
 
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, Rank2Types #-}
 
 import Control.Exception
 import System.Exit
@@ -43,23 +43,23 @@ naiveChunk n l
         take n l : naiveChunk n (drop n l)
 
 -- Exhaustively compare naive with actual chunk on small inputs.
-makeCorrespondings :: Int -> Int -> Test
-makeCorrespondings gMax nMax =
+makeCorrespondings :: (Int -> [Int] -> [[Int]]) -> Int -> Int -> Test
+makeCorrespondings f gMax nMax =
     TestList [makeCorresponding g n | g <- [1..gMax], n <- [1..nMax]]
     where
       makeCorresponding g n =
           "corresponding " ++ show g ++ "/" ++ show n ~:
-          naiveChunk g [1..n] ~=? chunk g [1..n]
+          naiveChunk g [1..n] ~=? f g [1..n]
 
 -- Law: concat (chunk g l) == l
 -- Exhaustively test for small inputs.
-makeFlattenings :: Int -> Test
-makeFlattenings gnMax =
+makeFlattenings :: (Int -> [Int] -> [[Int]]) -> Int -> Test
+makeFlattenings f gnMax =
     TestList [makeFlattening g n | g <- [1..gnMax], n <- [1..gnMax]]
     where
       makeFlattening g n =
           "flattening " ++ show g ++ "/" ++ show n ~:
-          [1..n] ~=? concat (chunk g [1..n])
+          [1..n] ~=? concat (f g [1..n])
 
 nill :: [[()]]
 nill = []
@@ -67,24 +67,33 @@ nill = []
 badList :: [Int]
 badList = [1..5] ++ [undefined] ++ [7..10]
 
+testF :: (forall a . Int -> [a] -> [[a]]) -> String -> Test
+testF f fName =
+    let mfName = "Data.List." ++ fName
+        mfMsg = mfName ++ ": non-positive count" in
+    TestList [
+      "specials" ~: TestList [
+        "0 on empty list" ~:
+          TestCase (assertExpectException mfName
+                    (ErrorCall mfMsg)
+                    (evaluate $ f 0 [])),
+        "0 on singleton list" ~:
+          TestCase (assertExpectException mfName
+                    (ErrorCall mfMsg)
+                    (evaluate $ f 0 [()])),
+        "empty list" ~: [] ~=? f 1 nill ],
+      "corresponding" ~: makeCorrespondings f 5 9,
+      "flattenings" ~: makeFlattenings f 9,
+      "strictness" ~: TestList [
+        "strict ahead" ~: 3 ~=? f 3 badList !! 0 !! 2,
+        "strict inside" ~: 3 ~=? f 7 badList !! 0 !! 2,
+        "strict behind" ~: 9 ~=? f 7 badList !! 1 !! 1 ] ]
+
 tests :: Test
 tests = TestList [
-         "specials" ~: TestList [
-           "0 on empty list" ~:
-             TestCase (assertExpectException "Data.List.chunk"
-                      (ErrorCall "Data.List.chunk: non-positive count")
-                      (evaluate $ chunk 0 [])),
-           "0 on singleton list" ~:
-             TestCase (assertExpectException "Data.List.chunk"
-                      (ErrorCall "Data.List.chunk: non-positive count")
-                      (evaluate $ chunk 0 [()])),
-           "empty list" ~: [] ~=? chunk 1 nill ],
-         "corresponding" ~: makeCorrespondings 5 9,
-         "flattenings" ~: makeFlattenings 9,
-         "strictness" ~: TestList [
-           "strict ahead" ~: 3 ~=? chunk 3 badList !! 0 !! 2,
-           "strict inside" ~: 3 ~=? chunk 7 badList !! 0 !! 2,
-           "strict behind" ~: 9 ~=? chunk 7 badList !! 1 !! 1 ] ]
+         "chunk" ~: testF chunk "chunk",
+         "chunk_splitat" ~: testF chunk_splitat "chunk_splitat",
+         "chunk_naive" ~: testF chunk_naive "chunk_naive" ]
 
 main :: IO ()
 main = do
